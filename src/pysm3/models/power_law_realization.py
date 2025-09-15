c95f1be79ca23d7522f0cb7943ba3099ff87ecb4
mab68 <mark@bishop.kiwi.nz>
2025-09-15 09:57:10 -0700

    add multifractal field realizations

--- src/pysm3/models/power_law_realization.py
+++ src/pysm3/models/power_law_realization.py
@@ -25,6 +25,8 @@ class PowerLawRealization(PowerLaw):
         synalm_lmax=None,
         has_polarization=True,
         map_dist=None,
+        lamb=None,
+        skew=None,
     ):
         """PowerLaw model with stochastic small scales
 
@@ -103,50 +105,122 @@ class PowerLawRealization(PowerLaw):
             u.dimensionless_unscaled
         )
         self.nside = int(nside)
+
+        if lamb is None or skew is None:
+            print('generating monofractal field')
+            small_scale = self.draw_realization(synalm_lmax, seeds)
+        else:
+            print('generating multifractal field')
+            small_scale = self.draw_mff_realization(lamb, skew, synalm_lmax, seeds)
+        self.f_real = small_scale.copy()
         (
             self.I_ref,
             self.Q_ref,
             self.U_ref,
             self.pl_index,
-        ) = self.draw_realization(synalm_lmax, seeds)
+        ) = self.modulate_small_scales(small_scale, synalm_lmax, seeds)
 
-    def draw_realization(self, synalm_lmax=None, seeds=None):
-
+    def draw_mff_realization(self, lamb, skew, synalm_lmax=None, seeds=None):
         if seeds is None:
             seeds = (None, None)
-
         if synalm_lmax is None:
             synalm_lmax = int(min(16384, 2.5 * self.nside))
-
         output_lmax = int(min(synalm_lmax, 2.5 * self.nside))
-
         np.random.seed(seeds[0])
 
+        Nfields = 3
+        corr_ell = 2.
+        var1 = [1. for _ in range(Nfields)]
+        width = 1.
+        ell = np.float64(np.arange(len(self.small_scale_cl[0])))
+        var2 = [np.nansum((2.*ell+1.)*Cl)/(4.*np.pi) for Cl in self.small_scale_cl]
+
+        ## Generate correlated Gaussian white noise
+        # Random phases (uncorrelated white noise)
+        omega_alm = [hp.synalm(np.ones(synalm_lmax)) for i in range(Nfields)]
+        ell = np.float64(np.arange(synalm_lmax))
+        filter_ell = 1./np.sqrt(ell**2 + corr_ell**2)
+        omega_alm = [hp.almxfl(omega_alm[i], filter_ell) for i in range(Nfields)]
+        omega_real = hp.alm2map(omega_alm, nside=self.nside)
+        curr_var = [np.nanmean(np.abs(omega_real[i])**2) for i in range(Nfields)]
+        omega_real = [np.sqrt(var1[i]) * omega_real[i] / np.sqrt(curr_var[i]) for i in range(Nfields)]
+        self.omega_real = omega_real.copy()
+
+        ## Generate volatility field
+        sigma_real = [np.exp(omega_real[i] * np.sqrt(lamb)) for i in range(Nfields)]
+        self.sigma_real = sigma_real.copy()
+
+        ## Symmetrize the volatility field
+        from scipy.stats import skewnorm
+        curr_var = [np.nanmean(np.abs(sigma_real[i])**2) for i in range(Nfields)]
+        dh_skew = [skewnorm.rvs(skew, 0, np.sqrt(curr_var[i])*width, np.shape(sigma_real[i])) for i in range(Nfields)]
+        dh_real = [dh_skew[i] * sigma_real[i] for i in range(Nfields)]
+        self.dh_real = dh_real.copy()
+
+        ## Generate multifractal field
+        dh_alm = hp.map2alm(dh_real, lmax=output_lmax)
+        # Use fractional derivative from small scale Cl
+        filter_ell = [np.sqrt(Cl) for Cl in self.small_scale_cl]
+        f_alm = [hp.almxfl(dh_alm[i], filter_ell[i]) for i in range(Nfields)]
+        # Ensure no bad values generated
+        for _alm in f_alm:
+            _alm[np.isnan(_alm)|np.isinf(_alm)] = 0. + 1j * 0.
+        # Map to output lmax
+        f_alm = [hp.almxfl(f_alm[i], np.ones(output_lmax+1)) for i in range(Nfields)]
+        f_real = hp.alm2map(f_alm, nside=self.nside, lmax=output_lmax)
+        curr_var = [np.nanmean(np.abs(f_real[i])**2) for i in range(Nfields)]
+        f_real = np.array([np.sqrt(var2[i]) * f_real[i] / np.sqrt(curr_var[i]) for i in range(Nfields)])
+
+        return np.float64(f_real)
+
+    def draw_realization(self, synalm_lmax=None, seeds=None):
+        if seeds is None:
+            seeds = (None, None)
+        if synalm_lmax is None:
+            synalm_lmax = int(min(16384, 2.5 * self.nside))
+        output_lmax = int(min(synalm_lmax, 2.5 * self.nside))
+        np.random.seed(seeds[0])
+
+        ## Synthesize Cl
+        # tt, ee, bb, te
+        # -> t, e, b
         alm_small_scale = hp.synalm(
             list(self.small_scale_cl.value),
             lmax=synalm_lmax,
             new=True,
         )
-
         alm_small_scale = [
             hp.almxfl(each, np.ones(output_lmax + 1)) for each in alm_small_scale
         ]
+        # t, e, b
+        # -> i, q, u
         map_small_scale = hp.alm2map(alm_small_scale, nside=self.nside)
+        return map_small_scale
 
-        # need later for beta
+    def modulate_small_scales(self, map_small_scale, synalm_lmax=None, seeds=None):
+        if seeds is None:
+            seeds = (None, None)
+        if synalm_lmax is None:
+            synalm_lmax = int(min(16384, 2.5 * self.nside))
+        output_lmax = int(min(synalm_lmax, 2.5*self.nside))
+        np.random.seed(seeds[0])
+
+        # NOTE: need later for beta
         modulate_map_I = hp.alm2map(self.modulate_alm[0].value, self.nside)
-
+        ## Modulate the small-scale fluctuations by the modulation maps
         map_small_scale[0] *= modulate_map_I
         map_small_scale[1:] *= hp.alm2map(self.modulate_alm[1].value, self.nside)
-
         if len(self.modulate_alm) == 3:
+            ## If modulate_alm == 3, then the third index is the beta, rather than the first
             modulate_map_I = hp.alm2map(self.modulate_alm[2].value, self.nside)
 
+        ## Add large scale/mean field
         map_small_scale += hp.alm2map(
             self.template_largescale_alm.value,
             nside=self.nside,
         )
 
+        ## Convert to IQU
         output_IQU = (
             utils.log_pol_tens_to_map(map_small_scale)
             * self.template_largescale_alm.unit
@@ -171,6 +245,7 @@ class PowerLawRealization(PowerLaw):
             new=True,
         )
 
+        ## Generate a Gaussian power law field
         alm_small_scale = hp.almxfl(alm_small_scale, np.ones(output_lmax + 1))
         pl_index = hp.alm2map(alm_small_scale, nside=self.nside) * output_unit
         pl_index *= modulate_map_I
